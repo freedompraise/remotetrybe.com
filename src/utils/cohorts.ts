e816a0ebe53da2f4ff6da24f5027630e72ef904b
Praise Freedom Dike <70984186+freedompraise@users.noreply.github.com>
2026-01-09 22:11:21 +0100

    Update date in cohorts.ts to 15 may from 22

--- src/utils/cohorts.ts
+++ src/utils/cohorts.ts
@@ -30,7 +30,7 @@ export const cohorts: Cohort[] = [
     name: "Cohort 9",
     registrationStart: "2026-03-16",
     registrationEnd: "2026-05-10",
-    trainingStart: "2026-05-22",
+    trainingStart: "2026-05-15",
     trainingEnd: "2026-06-21",
     whatsappLink: import.meta.env.VITE_COHORT9_WA_LINK as string,
     paystackProductUrl: "https://paystack.com/buy/copy-2-of-virtual-assistant-masterclass-by-remotetryb-qmnnqw"
